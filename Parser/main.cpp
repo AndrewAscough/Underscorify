22a088e7c4c996f428258a1dd79550ba99676592
AndrewAscough <a1706465@student.adelaide.edu.au>
2020-03-22 17:45:27 +1030

    Program now takes filename in from command line upon running. Will error if no filename is passed in

--- Parser/main.cpp
+++ Parser/main.cpp
@@ -3,23 +3,34 @@
 #include <sstream>
 #include <fstream>
 
-//Curently this prompts the person on running the program to input a file name, then prints each word onto a new line
-//Next goal is to pass in the file name thru the running of the program. Ie ./test test1.txt
-int main()
+//So now currently this program takes in multiple files to be underscorified in the command line
+//and outputs using stdout
+//I think next before I go further I would like to create a bash file to assist in testing this.
+int main(int argc, char *argv[])
 {
-	std::string fileName;
-	std::cin>>fileName;
-	
-	std::ifstream inputFile;
-	inputFile.open(fileName.c_str());
-	
-
-	while(inputFile >> word)
+	//Check if there is no filename being given
+	if(argc < 2)
 	{
-		std::cout<<word<<std::endl;
+		std::cout<<"Error, use is ./Underscorify [filename]"<<std::endl;
+		return 1;
+	}
+
+	//Opens each file and outputs the words on a new line
+	for(int i=1;i<argc;i++)
+	{
+		std::ifstream inputFile;
+		inputFile.open(argv[i]);
+
+		std::string word;
+
+		while(inputFile >> word)
+		{
+			std::cout<<word<<std::endl;
+		}
+		
+		inputFile.close();
 	}
 	
-	inputFile.close();
-
 	return 0;
+	
 }
