1233821321ca2daf0d6b1937bec71b8a61e524b3
AndrewAscough <a1706465@student.adelaide.edu.au>
2020-03-22 17:26:48 +1030

    Achieved first goal of taking in a filename to be parsed and printed each new word on a new line

--- Parser/main.cpp
+++ Parser/main.cpp
@@ -1,6 +1,25 @@
 #include <iostream>
+#include <string>
+#include <sstream>
+#include <fstream>
 
+//Curently this prompts the person on running the program to input a file name, then prints each word onto a new line
+//Next goal is to pass in the file name thru the running of the program. Ie ./test test1.txt
 int main()
 {
+	std::string fileName;
+	std::cin>>fileName;
+	
+	std::ifstream inputFile;
+	inputFile.open(fileName.c_str());
+	
+
+	while(inputFile >> word)
+	{
+		std::cout<<word<<std::endl;
+	}
+	
+	inputFile.close();
+
 	return 0;
 }
