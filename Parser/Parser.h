f6b7208dc91acae70ac17dedeca5c5d8f0310eb9
AndrewAscough <a1706465@student.adelaide.edu.au>
2020-03-27 16:15:35 +1030

    fixed bug where the program couldnt handle seeing // or /* or */ encased in a string and would freak out. Also fixed it detecting _ as a symbol instead of an alphanumeric character. and fixed it handling a #include <example> //this comment broke it. Finally, added the shell files to turn this into a class

---
+++ Parser/Parser.h
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <fstream>
+
+class Parser
+{
+	public:
+	private:	
+};
--- Parser/main.cpp
+++ Parser/main.cpp
@@ -59,9 +59,10 @@ bool isSymbolWord(std::string s)
 bool isSymbol(char c)
 {
 
-	if( (c>=48 && c<=57 ) ||
+	if( (c>=48 && c<=57) ||
 		(c>=65 && c<=90) ||
-		(c>=97 && c<=122))
+		(c>=97 && c<=122)||
+		 c==95)
 		{
 			return false;
 		}
@@ -233,8 +234,14 @@ std::string extractBlockComment(std::string line)
 
 	for(int i=0;i<line.length();i++)
 	{
-		//String block has encountered a comment beginning and up until now the buffer contains non commented strings. The iscomment%2 is used here to ensure we dont have a line like /* foo/* bar*/
-		if(	(line[i] == '/' && line[i+1] == '*') && isComment%2 == 0)
+		if(line[i] == 34 || line[i] == 39)
+		{
+			result+=spaceifySymbolWord(buffer);
+			buffer = "";
+			isString++;
+		}
+		//Word block has encountered a comment beginning and up until now the buffer contains non commented words. The iscomment%2 is used here to ensure we dont have a line like /* foo/* bar*/
+		else if(	(line[i] == '/' && line[i+1] == '*') && isComment%2 == 0 && isString%2 == 0)
 		{
 			//Add current word to result after separating out the symbolwords
 			result+= spaceifySymbolWord(buffer);
@@ -247,11 +254,11 @@ std::string extractBlockComment(std::string line)
 			result+=line[i];
 			i++;
 
-			//Increment our iscomment to notify the main program that we are now handling a comment string.
+			//Increment our iscomment to notify the main program that we are now handling a comment
 			isComment++;
 		}
-		//String block has encountered a comment end. The buffer now contains unmodified strings from the comment
-		else if( (line[i] == '*' && line[i+1] == '/') && isComment%2 == 1)
+		//Word block has encountered a comment end. The buffer now contains unmodified strings from the comment
+		else if( (line[i] == '*' && line[i+1] == '/') && isComment%2 == 1 && isString%2 == 0)
 		{
 			//add buffer to result
 			result+= buffer;
@@ -277,14 +284,35 @@ std::string extractBlockComment(std::string line)
 			result+=extractLineComment(line.substr(i,line.length()-i));
 			break;
 		}
-		//Adds current char to buffer, buffer will be modified each time isBlockComment returns true.
-		buffer+=line[i];
+		
+		//if line is currently not within a string, add the char to buffer, else it will go to the result.
+		if(isString%2 ==0)
+		{
+			buffer+=line[i];
+		}
+		else
+		{
+			result+=line[i];
+		}
 	}
-	if(isComment%2==1)
-	{
-		result+=buffer;
-	}
+
+	result+=buffer;
 	return result;
+}
+
+//adds "out" before the . in a normal file (if there is a dot). If there isnt it will add it to the end of the filename
+std::string nameOutputFile(std::string s)
+{
+	for(int i=0;i<s.length();i++)
+	{
+		if(s[i] == '.')
+		{
+			s.insert(i,"out");
+			return s;
+		}
+	}
+	s+="out";
+	return s;
 }
 
 int main(int argc, char *argv[])
@@ -301,6 +329,10 @@ int main(int argc, char *argv[])
 	{
 		std::ifstream inputFile;
 		inputFile.open(argv[i]);
+		
+		//Generate output file
+		std::string inputFileName(argv[i]);
+		std::ofstream outputFile(nameOutputFile(inputFileName).c_str());
 
 		std::string line;
 		
@@ -310,52 +342,49 @@ int main(int argc, char *argv[])
 			std::string word;
 			std::istringstream currentLine(line);
 
-			//Check if the word contains a block comment OR if it is currently still in one. Also handles when there is both a line comment and block comment on the same line
-			if(isBlockComment(line) || isComment%2 == 1)
+			//Preprocessor directive thingy, essentially not something I want to mess with. Think #include <iostream> AND this isnt found in a comment
+			if(line[0] == '#' && isComment%2 ==0)
 			{
-				std::cout<<extractBlockComment(line)<<std::endl;
+				outputFile<<line<<std::endl;
+			}
+			//Check if the word contains a block comment OR if it is currently still in one and that it isnt in astring. Also handles when there is both a line comment and block comment on the same line
+			else if((isBlockComment(line) || isComment%2 == 1) && isString%2 == 0)
+			{
+				std::cerr<<"isString was: " << isString << " line was: " << line << std::endl;
+				outputFile<<extractBlockComment(line)<<std::endl;
 			}
 			//Check if the REST of the line is a // comment
 			else if(isLineComment(line))
 			{
-				std::cout<<extractLineComment(line)<<std::endl;
+				outputFile<<extractLineComment(line)<<std::endl;
 			}
 			//Line does not contain a comment so must be examined
 			else
 			{
 				while(currentLine >> word)
 				{
-					//Preprocessor directive thingy, essentially not something I want to mess with. Think #include <iostream>
-					if(word[0] == '#')
-					{
-						std::cout<<word<<" ";
-						while(currentLine>>word)
-						{
-							std::cout<<word<<" ";
-						}
-						std::cout<<std::endl;
-						break;
-					}
 					//for some resemblance of "formatting" when a ; is encountered it will put it on a new line
-					else if(newLineNeeded(word))
+					if(newLineNeeded(word))
 					{
-						std::cout<<spaceifySymbolWord(word)<<std::endl;
+						outputFile<<spaceifySymbolWord(word)<<std::endl;
 					}
 					//Handles breaking up symbol words and passing through strings without changing the interior.
 					else if(isSymbolWord(word))
 					{
-						std::cout<<spaceifySymbolWord(word);		
+						outputFile<<spaceifySymbolWord(word);		
 					}
 					//Word is not a symbol word, it can remain unchanged.
 					else
 					{
-						std::cout<<word<<" ";
+						outputFile<<word<<" ";
 					}
 				}
 			}
 		}
 
 		inputFile.close();
+		outputFile<<std::endl;
+		outputFile.close();
 	}
 	
 	return 0;
